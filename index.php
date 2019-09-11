1f12bbc06b7fd44a6f302d8e8595dba489c3d550
Sadiqur Rahman Rifat <you@example.com>
2019-09-11 23:12:29 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,280 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+	
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	 <link rel="stylesheet" type="text/css" href="style.css">
+	 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+
+	
+	<title>podua</title>
+	
+<style>
+
+  </style>
+</head>
+<body>
+
+
+<div class="container-fluid">
+	<div class="header" style="padding-bottom: 20px;">
+      
+      <nav class="navbar navbar-light">
+        <a class="navbar-brand" href="index.php">
+          <i class="fa fa-cogs font-weight-bold" style="color: blue;"></i>
+          <b style="color: blue;">পড়ুয়া </b>
+        </a>
+        <div class="search-box">
+          <input class="search-txt" type="text" name="" placeholder="Search heare..."  style="margin-left: 10px; ">
+          <a class="search-btn" href="">
+          <i class="fas fa-search"></i>
+          </a>
+        </div>
+
+        <div class="notify-bar">
+        <i style="margin-right: 15px" class="far fa-envelope"></i>
+        <i style="margin-right: 15px" class="fa fa-bell"></i>
+        <i style="margin-right: 15px" class="fa fa-user"></i>
+        </div>
+      </nav>
+    </div>
+    <div class="d-md-flex h-md-100">
+
+<!-- First Half -->
+
+<div class="saidbar col-md-2 p-0  h-md-100">
+    <div class="nav-item">
+        <div class="font-weight-normal">
+           <ul class="list-unstyled components">
+            
+            <li class="">
+                <a href="" class=""><i class="fa fa-home" style="margin-right: 10px"></i>Home</a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-address-card"style="margin-right: 10px"></i>About</a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-file"style="margin-right: 10px"></i>Pages</a>
+                
+            </li>
+            
+            <li>
+                <a href="#"><i class="far fa-address-book"style="margin-right: 10px"></i>Contact</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</div>
+
+<!-- Second Half -->
+
+<div class="body col-md-10 h-md-100">
+    <nav class="navbar navbar-expand-lg list-unstyled">
+      <li>
+        <a class="navbar-brand " href="#"  style="padding: 0px 20px 0px 20px; font-size: small;">Accademic</a>
+        <a class="navbar-brand " href="#"  style="padding: 0px 20px 0px 20px;font-size: small;">History</a>
+        <a class="navbar-brand " href="#"  style="padding: 0px 20px 0px 20px;font-size: small;">General Knowledge</a>
+        <a class="navbar-brand " href="#"  style="padding: 0px 20px 0px 20px;font-size: small;">Religion</a>
+        <a class="navbar-brand " href="#"  style="padding: 0px 20px 0px 20px;font-size: small;">Ryems</a>
+        <a class="navbar-brand " href="#"  style="padding: 0px 20px 0px 20px;font-size: small;">New Paper</a>
+      </li>
+  </nav>
+ <div class="container">
+  <h6>Popular books<h6><br>
+  <div class="row">
+    <div class="cal1 col-sm bg-light">
+      <div class="row" >
+        <img src="image/hek1.jfif" alt="..." class="col-sm-5" style="padding-left: 0px; border-radius: 25px;">
+          
+        <p class="font-weight-bold" style="margin-top: 10px;">চায়ের পিয়ালা <br>
+          <br>
+          <span style="font-size: small; color: gray;">Writer</span><br>
+          <span style="font-size: normal; color:black;"> হরিপদো দাস </span>
+          
+          <div class="col">
+            <div class="progress" style="width: 50px; background: gray; height: 8px; margin-top: 50px;">
+            <div class="progress-bar" style="color: yellow; width: 92%; font-size: smaller;" role="progressbar" aria-valuenow="22" aria-valuemin="0" aria-valuemax="20">
+            </div>
+          </div>
+        </div>
+          
+        </p>
+         
+      </div>
+      
+    </div> 
+
+
+ <div class="cal1 col-sm bg-light">
+      <div class="row" >
+        <img src="image/hek.png" alt="..." class="col-sm-5" style="padding-left: 0px; border-radius: 25px;">
+          
+        <p class="font-weight-bold" style="margin-top: 10px;">পৃথিবী ঘুরে <br>
+          <br>
+          <span style="font-size: small; color: gray;">Writer</span><br>
+          <span style="font-size: normal; color:black;"> নাইকো রহমান </span>
+          
+          <div class="col">
+            <div class="progress" style="width: 50px; background: gray; height: 8px; margin-top: 50px;">
+            <div class="progress-bar" style="color: yellow; width: 80%; font-size: smaller;" role="progressbar" aria-valuenow="22" aria-valuemin="0" aria-valuemax="20">
+            </div>
+          </div>
+        </div>
+          
+        </p>
+         
+      </div>
+      
+    </div>
+    <div class="cal1 col-sm bg-light">
+      <div class="row" >
+        <img src="image/test.jfif" alt="..." class="col-sm-5" style="padding-left: 0px; border-radius: 25px;">
+          
+         <p class="font-weight-bold" style="margin-top: 10px;">ক্যামেরাই সব নয় <br>
+          <br>
+          <span style="font-size: small; color: gray;">Writer</span><br>
+          <span style="font-size: normal; color:black;">রাজিব বিন আজাদি</span>
+          
+          <div class="col">
+            <div class="progress" style="width: 40px; background: gray; height: 8px; margin-top: 50px;">
+            <div class="progress-bar" style="color: yellow; width: 77%; font-size: smaller;" role="progressbar" aria-valuenow="22" aria-valuemin="0" aria-valuemax="20">
+            </div>
+          </div>
+        </div>
+          
+        </p>
+         
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <h6>New books<h6><br>
+  <div class="row">
+    <div class="cal1 col-sm bg-light">
+      <div class="row" >
+        <img src="image/art.jfif" alt="..." class="col-sm-5" style="padding-left: 0px; border-radius: 25px;">
+      
+         <p class="font-weight-bold" style="margin-top: 10px;">তুলিতে <br><br>
+          <span style="font-size: small; color: gray;">Writer</span><br>
+          <span style="font-size: normal; color:black;"> অক্সিজেন ফুয়াদ </span></p>
+              
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star "style="color: orange;"></span>
+                <span class="fa fa-star" style="color: orange;"></span>
+                <span class="fa fa-star" style="color: orange;"></span>
+              
+      </div>
+
+    </div>
+
+    <div class="cal2 col-sm bg-light">
+            <div class="row" >
+        <img src="image/flower.jfif" alt="..." class="col-sm-5" style="padding-left: 0px; border-radius: 25px;">
+      
+         <p class="font-weight-bold" style="margin-top: 10px;">ফুলের কাহিনী <br><br>
+          <span style="font-size: small; color: gray;">Writer</span><br>
+          <span style="font-size: normal; color:black;">কায়সান হাদি</span></p>
+
+          <span class="fa fa-star " style="color: orange;"></span>
+          <span class="fa fa-star " style="color: orange;"></span>
+          <span class="fa fa-star " style="color: orange;"></span>
+          <span class="fa fa-star" style="color: orange;"></span>
+          <span class="fa fa-star" style="color: orange;"></span>
+      </div>
+    </div>
+    <div class="cal3 col-sm bg-light">
+            <div class="row" >
+        <img src="image/tree.jfif" alt="..." class="col-sm-5" style="padding-left: 0px; border-radius: 25px;">
+      
+        <p class="font-weight-bold" style="margin-top: 10px;">রঙ্গিন বৃক্ষ <br><br>
+          <span style="font-size: small; color: gray;">Writer</span><br>
+          <span style="font-size: normal; color:black;"> স্মরনিকা হাদিয়া</span></p>
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star "style="color: orange;"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <h6>Business<h6><br>
+  <div class="row">
+    <div class="cal1 col-sm bg-light">
+      <div class="row" >
+        <img src="image/lef.jfif" alt="..." class="col-sm-5" style="padding-left: 0px; border-radius: 25px;">
+      
+        <p class="font-weight-bold" style="margin-top: 10px;">একটি সুযোগ <br><br>
+          <span style="font-size: small; color: gray;">Writer</span><br>
+          <span style="font-size: normal; color:black;">কর্মকার  </span></p>
+
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star "style="color: orange;"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+      </div>
+    </div>
+
+    <div class="cal2 col-sm bg-light">
+            <div class="row" >
+        <img src="image/images.jfif" alt="..." class="col-sm-5" style="padding-left: 0px; border-radius: 25px;">
+      
+        <p class="font-weight-bold" style="margin-top: 10px;">চিন্তার বাইরে <br><br>
+          <span style="font-size: small; color: gray;">Writer</span><br>
+          <span style="font-size: normal; color:black;"> সজল আহাম্মেদ </span></p>
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star "></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+      </div>
+    </div>
+    <div class="cal3 col-sm bg-light">
+            <div class="row" >
+        <img src="image/paint.jfif" alt="..." class="col-sm-5" style="padding-left: 0px; border-radius: 25px;">
+      
+        <p class="font-weight-bold" style="margin-top: 10px;">তুলি তেই রং <br><br>
+          <span style="font-size: small; color: gray;">Writer</span><br>
+          <span style="font-size: normal; color:black;"> নাইকো </span>
+        </p>
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star " style="color: orange;"></span>
+                <span class="fa fa-star "></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+    
+</div>
+
+  
+</div>
+	
+	
+
+
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+</body>
+</html> 
